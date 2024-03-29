94189060f29d9903ad2aa1c8d5a3e06a7d7c299e
mamun ethan <mamunsikder73375@gmail.com>
2024-03-29 09:19:14 +0600

    font-asewome icon,google fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1200px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    color: {
+      primary: "#FFFEF8",
+      secondary: "#141C3A",
+      accent: "#0000DC",
+    },
+
     extend: {},
   },
   plugins: [],
