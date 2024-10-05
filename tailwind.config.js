f0af14cc390f4d2d4a092ab1bc241b0759e97a46
mamun ethan <mamunsikder73375@gmail.com>
2024-10-05 13:16:04 +0600

    some animatin complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,22 @@ module.exports = {
       nic: ["Niconne", "system-ui"],
       pop: ["Poppins", "system-ui"],
     },
+
+    animation: {
+      subtleBounce: "subtleBounce 0.8s ease-in-out infinite",
+      wave: "wave 0.8s ease-in-out infinite",
+    },
+    keyframes: {
+      subtleBounce: {
+        "0%, 100%": { transform: "translateY(0)" },
+        "50%": { transform: "translateY(-5px)" },
+      },
+      wave: {
+        "0%, 100%": { transform: "rotate(0deg)" },
+        "50%": { transform: "rotate(20deg)" },
+      },
+    },
   },
+
   plugins: [],
 };
